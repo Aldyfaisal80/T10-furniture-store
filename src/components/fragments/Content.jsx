dc0e5427495f1d2f4e4cbf2d481f9d78667a4025
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-08-03 19:15:40 +0700

    setup

---
+++ src/components/fragments/Content.jsx
@@ -0,0 +1,18 @@
+export default function Content() {
+  return (
+    <div>
+        <div className="w-full h-screen border-2 border-red-500">
+            <h1>View High 1</h1>
+        </div>
+        <div className="w-full h-screen border-2 border-blue-500">
+            <h1>View High 2</h1>
+        </div>
+        <div className="w-full h-screen border-2 border-yellow-500">
+            <h1>View High 3</h1>
+        </div>
+        <div className="w-full h-screen border-2 border-green-500">
+            <h1>View High 4</h1>
+        </div>
+    </div>
+  )
+}
